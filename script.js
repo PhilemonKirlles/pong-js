7aaaa96336da91ba3f9755bfd9f6ab7b88fa023c
Replit user <>
2023-03-28 03:14:31 +0000

    Ai/ player scripting

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var rounds = [5,5,3,3,2];
 var colors = ["#d4336e", "#33d477", "#1abc9c", "#cc3e2e", "#8acc2e"]
 
 //the ball
-var ball {
+var Ball= {
   new: function (incrementedSpeed) {
     return {
       width: 18,
@@ -21,10 +21,13 @@ var ball {
       y: (this.canvas.height / 2) -9,
       moveX: DIRECTION.IDLE,
       moveY: DIRECTION.IDLE,
-    }
+      speed: incrementedSpeed || 7
+    };
     
   }
 };
+
+
 var Ai= {
   new: function (side) {
     return{
@@ -35,6 +38,43 @@ var Ai= {
       score: 0
       move: DIRECTION.IDLE,
       speed: 8
-    }
+    };
   }
-}
+};
+
+var Game = {
+  initialize: function () {
+    this.canvas = document.querySelector('canvas');
+    this.context = this.canvas.getContext('2d');
+    this.canvas.width = 1400;
+    this.canvas.height = 1000;
+
+    this canvas style.widht = (this.canvas.width /2) + "px";
+    this canvas style.height = (this.canvas.height / 2) + "px";
+
+    this.player = Ai.new.call(this, "left");
+    this.ai = Ai.new.call (this, "right");
+    this.ball = Ball.new.call(this);
+
+    this.ai.speed =5;
+    this.running = this.over = false;
+    this.turn = this.ai;
+    this.timer = this.round = 0;
+    this.color = '#8c52ff'
+
+    Pong.menu();
+    Pong.listen();
+  },
+
+  endGameMenu: function (text)
+    //change canvas font size/color
+    Pong.context.font = '40px Roboto';
+    Pong.context.fillStyle = this.color;
+
+    Pong.context.fillReact 
+      Pong.canvas.width / 2 -350,
+      Pong.canvas.height /2 -48, 
+      700,
+      100
+    
+};
